a826b2f64407c7eb79d93b27252d03d1558ca318
Gnak <523748995@qq.com>
2024-07-01 09:54:05 +0800

    fix：调整配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://zzkb.top",
   base: "/",
   trailingSlash: "always",
   integrations: [tailwind(), swup({
