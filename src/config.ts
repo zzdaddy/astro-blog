1bb03ce9251b4a703eead3b9d2bc6deff1894af5
Gnak <523748995@qq.com>
2024-07-08 15:16:02 +0800

    feat: 更新rss

--- src/config.ts
+++ src/config.ts
@@ -9,23 +9,24 @@ import { LinkPreset } from './types/config'
 export const siteConfig: SiteConfig = {
   title: '早早集市',
   subtitle: 'online',
-  lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja'
+  lang: 'zh_CN', // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
+    hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false, // Hide the theme color picker for visitors
   },
   banner: {
     enable: true,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    src: 'assets/images/demo-banner.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center', // Equivalent to object-position, defaults center
   },
-  favicon: [    // Leave this array empty to use the default favicon
+  favicon: [
+    // Leave this array empty to use the default favicon
     // {
     //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
     //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
     //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     // }
-  ]
+  ],
 }
 
 export const navBarConfig: NavBarConfig = {
@@ -35,22 +36,22 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/zzdaddy',     // Internal links should not include the base path, as it is automatically added
-      external: true,                               // Show an external link icon and will open in a new tab
+      url: 'https://github.com/zzdaddy', // Internal links should not include the base path, as it is automatically added
+      external: true, // Show an external link icon and will open in a new tab
     },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/zzstudio-logo.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: 'assets/images/zzstudio-logo.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'zzdaddy',
   bio: '今天是二手商贩！',
   links: [
     {
       name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
-                                        // You will need to install the corresponding icon set if it's not already included
-                                        // `pnpm add @iconify-json/<icon-set-name>`
+      icon: 'fa6-brands:twitter', // Visit https://icones.js.org/ for icon codes
+      // You will need to install the corresponding icon set if it's not already included
+      // `pnpm add @iconify-json/<icon-set-name>`
       url: 'https://twitter.com',
     },
     {
